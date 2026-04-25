88a7ebafce35d455016496d483027a32fe0d081d
YuleTech Deploy <deploy@yuletech.community>
2026-04-25 08:41:06 +0800

    chore: create shared-utils and shared-types packages

---
+++ packages/shared-types/src/index.ts
@@ -0,0 +1,11 @@
+// Shared type definitions will be added here as needed
+export interface User {
+  id: string
+  name: string
+  email: string
+}
+
+export interface RouteConfig {
+  path: string
+  component: string
+}
